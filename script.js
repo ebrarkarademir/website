da3106fe4e1fd263eee31860d3db148719b3c785
Ebrar Karademir <ebrarcode@hotmail.com>
2023-11-05 23:29:28 +0300

    learn more button added

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function toggleDetails(button) {
+  var details = button.nextElementSibling; // This gets the .additional-details div
+  
+  // Check if the details are already expanded
+  var isExpanded = details.style.display !== 'none';
+  
+  // Toggle the display property
+  if (isExpanded) {
+    details.style.display = 'none';
+    button.innerText = 'Learn More';
+  } else {
+    details.style.display = 'block';
+    button.innerText = 'Learn Less';
+  }
+}
+
+function loadHTMLModule(url, elementId) {
+  fetch(url).then(function(response) {
+    return response.text();
+  }).then(function(html) {
+    document.getElementById(elementId).innerHTML = html;
+  }).catch(function(err) {
+    console.warn('Something went wrong.', err);
+  });
+}
